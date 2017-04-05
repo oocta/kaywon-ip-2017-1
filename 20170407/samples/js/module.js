e456e2f36eae397ea30ebd3a1b3dace167c6c667
--global <super.ultra.soft@gmail.com>
2017-04-06 01:45:13 +0900

    Week6(feat): Make module.js and include in index.html

---
+++ 20170407/samples/js/module.js
@@ -0,0 +1,13 @@
+/**
+ * 매개변수로 입력된 엘리먼트를 복제한 엘리먼트를 반환한다.
+ * @param {Element} element
+ */
+function createH2tag(text) {
+  var h2 = document.createElement('h2');
+  h2.innerText = text;
+  return h2;
+}
+
+var h1 = document.getElementsByTagName('h1')[0];
+var h2 = createH2tag('yolo!');
+document.body.appendChild(h2);
