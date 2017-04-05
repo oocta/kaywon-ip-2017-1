b9040cf04f7440840ab48fe631db6779403166ef
--global <super.ultra.soft@gmail.com>
2017-04-06 01:58:27 +0900

    Week(feat): Add 'cloneElement' method

--- 20170407/samples/js/module.js
+++ 20170407/samples/js/module.js
@@ -4,5 +4,10 @@ var module = {
     var h2 = document.createElement('h2');
     h2.innerText = text;
     document.body.appendChild(h2);
+  },
+  cloneElement: function (originElement) {
+    if (originElement.nodeType === 1) {
+      return originElement.cloneNode(true);
+    }
   }
 };
