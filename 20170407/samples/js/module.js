9bdbac9aa0ff0d2a4d4c84401b43a8676da16d02
--global <super.ultra.soft@gmail.com>
2017-04-06 01:57:46 +0900

    Week6(feat): Create module object

--- 20170407/samples/js/module.js
+++ 20170407/samples/js/module.js
@@ -1,9 +1,8 @@
-/**
- * 매개변수로 입력된 엘리먼트를 복제한 엘리먼트를 반환한다.
- * @param {Element} element
- */
-function createH2tag(text) {
-  var h2 = document.createElement('h2');
-  h2.innerText = text;
-  document.body.appendChild(h2);
-}
+var module = {
+  target: null,
+  createH2tag: function (text) {
+    var h2 = document.createElement('h2');
+    h2.innerText = text;
+    document.body.appendChild(h2);
+  }
+};
