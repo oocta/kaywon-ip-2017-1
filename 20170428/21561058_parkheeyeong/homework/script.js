1161644c25e804b074d96b84333a4f02f1fc0248
tongtong9513 <tongtong9513@gmail.com>
2017-05-10 23:53:08 +0900

    과제제출합니다

--- 20170428/21561058_parkheeyeong/homework/script.js
+++ 20170428/21561058_parkheeyeong/homework/script.js
@@ -12,35 +12,126 @@ var radio3 = increment[0].childNodes[5];
 var radio4 = increment[0].childNodes[7];
 var radio5 = increment[0].childNodes[9];
 
-//라디오체크값 가져오기
-var count=increment.length;
-
-for(var i =0;i<count;i++){
-  if(increment[i].cheked){
-    var value = increment[i].value;
-  }
-}
-
-
+// //라디오체크값 가져오기 - 둘다 실패했습니다..ㅠㅠ
+// var count=increment.length;
+//
+//
+// for(var i =0;i<count;i++){
+//   if(increment[i].cheked){
+//     var value = increment[i].value;
+//   }
+// }
 // //라디오 체크값 검색해본 결과 - 실행안됌
 // function plusNum(){
-//   var num = 0;
-//   if(document.dataForm.radio1.checked==true)
+//   var count = 0;
+//   if(document.increment.radio1.checked==true)
 //   { counter.innerHTML = '';
-//   ++num;
-//   counter.innerHTML = num;
+//   ++count;
+//   counter.innerHTML = count;
 //   return;}
 // }
 //
 // plusbtn.addEventListener('click', plusNum)
 
-var num = 0;
+//넣을 숫자와 라디오체크 초기값 주기
+var value = 0;
+var count = 0;
+
+// //숫자를 증가시키는 함수선언
+// function plusNum() {
+//   if (value === 1) {
+//     count = ++count;
+//     counter.innerHTML = count;
+//   }
+//   else if (value === 2) {
+//     count = count + 2;
+//     counter.innerHTML = count;
+//   }
+//   else if (value === 3) {
+//     count = count + 3;
+//     counter.innerHTML = count;
+//   }
+//   else if (value === 4) {
+//     count = count + 4;
+//     counter.innerHTML = count;
+//   }
+//   else  {
+//     count = count + 5;
+//     counter.innerHTML = count;
+//   }
+// }
 //숫자를 증가시키는 함수선언
-function plusNum(){
-  if (radiobtn == 1){
-      num = ++num;
-      counter.innerHTML = [num];
-    }
+plusbtn.addEventListener('click',function() {
+  if (value === 1) {
+    count = ++count;
+    counter.innerHTML = count;
+  }
+  else if (value === 2) {
+    count = count + 2;
+    counter.innerHTML = count;
+  }
+  else if (value === 3) {
+    count = count + 3;
+    counter.innerHTML = count;
+  }
+  else if (value === 4) {
+    count = count + 4;
+    counter.innerHTML = count;
+  }
+  else if (value === 5){
+    count = count + 5;
+    counter.innerHTML = count;
+  }
+})
+
+minusbtn.addEventListener('click',function() {
+  if (value === 1) {
+    count = --count;
+    counter.innerHTML = count;
+  }
+  else if (value === 2) {
+    count = count - 2;
+    counter.innerHTML = count;
+  }
+  else if (value === 3) {
+    count = count - 3;
+    counter.innerHTML = count;
+  }
+  else if (value === 4) {
+    count = count - 4;
+    counter.innerHTML = count;
+  }
+  else if (value === 5){
+    count = count - 5;
+    counter.innerHTML = count;
+  }
+})
+
+//라디오값 주기
+var checking1 = function() {
+  value = 1;
+  return value;
+}
+var checking2 = function() {
+  value = 2;
+  return value;
+}
+var checking3 = function() {
+  value = 3;
+  return value;
+}
+var checking4 = function() {
+  value = 4;
+  return value;
+}
+var checking5 = function() {
+  value = 5;
+  return value;
 }
 
-plusbtn.addEventListener('click', plusNum)
+//클릭시 값을 전환
+radio1.addEventListener('click', checking1);
+radio2.addEventListener('click', checking2);
+radio3.addEventListener('click', checking3);
+radio4.addEventListener('click', checking4);
+radio5.addEventListener('click', checking5);
