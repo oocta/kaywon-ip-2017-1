cbd18a27250710acd98de26ef979f4c6186682bd
oocta <super.ultra.soft@gmail.com>
2017-05-12 09:45:26 +0900

    week10(feat): 버튼제어 기능 구조 만들기

--- 20170428/homework/script.js
+++ 20170428/homework/script.js
@@ -1,8 +1,25 @@
-//1. element 찾기
+//3. 버튼 제어 기능 만들기
 var init = function () {
   var counter = document.getElementById('counter');
   var plusButton = document.getElementById('plusButton');
   var minusButton = document.getElementById('minusButton');
+
+  //카운터를 제어하는 함수
+  function counterHandler() {
+    counter.innerHTML = '1';
+    console.log('counterHandler');
+  }
+
+  //플러스 버튼 클릭시 실행되는 함수
+  function clickPlusButton() {
+    counterHandler();
+  }
+
+  //마이너스버튼 클릭시 실행되는 함수
+  function clickMinusButton() {
+    counterHandler();
+  }
+
   console.log(plusButton, minusButton);
 };
 document.addEventListener('DOMContentLoaded', init);
