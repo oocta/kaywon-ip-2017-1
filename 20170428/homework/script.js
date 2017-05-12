baaf316b1f8a486f590bb17884a33c0c7c2cf1e9
oocta <super.ultra.soft@gmail.com>
2017-05-12 10:06:03 +0900

    week10(feat): 카운터 기능 추가

--- 20170428/homework/script.js
+++ 20170428/homework/script.js
@@ -1,25 +1,27 @@
-//3. 버튼 제어 기능 만들기
+//4. 카운터기능 추가
 var init = function () {
   var counter = document.getElementById('counter');
   var plusButton = document.getElementById('plusButton');
   var minusButton = document.getElementById('minusButton');
 
   //카운터를 제어하는 함수
-  function counterHandler() {
-    counter.innerHTML = '1';
+  function counterHandler(num) {
+    counter.innerHTML = num;
     console.log('counterHandler');
   }
 
   //플러스 버튼 클릭시 실행되는 함수
   function clickPlusButton() {
-    counterHandler();
+    counterHandler(1);
   }
 
   //마이너스버튼 클릭시 실행되는 함수
   function clickMinusButton() {
-    counterHandler();
+    counterHandler(-1);
   }
 
-  console.log(plusButton, minusButton);
+  plusButton.addEventListener('click', clickPlusButton);
+  minusButton.addEventListener('click', clickMinusButton);
+
 };
 document.addEventListener('DOMContentLoaded', init);
