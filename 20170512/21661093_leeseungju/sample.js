67ec90ffcaeb375bd20776eb20b26435aea5286d
adoablej <ju_9411@naver.com>
2017-05-12 12:37:43 +0900

    Update sample.js

--- 20170512/21661093_leeseungju/sample.js
+++ 20170512/21661093_leeseungju/sample.js
@@ -1,107 +1,48 @@
-//유효범위(Scope) - 1
-var msg = 'Good bye';
-var hello = function () {
-  var msg = 'Hello world';
-  console.log(msg);
-};
-
-console.log(msg);
-hello();
-
-//유효범위(Scope) - 2
-var msg = 'Good bye';
-var hello = function () {
-  msg = 'Hello world';
-  console.log(msg);
-};
-
-//호이스팅 (hoisting) - 1
-hi();
-var hi = function () {
-  console.log(hi);
-};
-hi();
-
-//호이스팅 (hoisting) - 2
-hi();
-function hi() {
-  console.log(hi);
-}
-hi();
-
-//this - 1
-console.log(this);
-
-//this - 2
-var printThis = function printThis() {
-  console.log('this', this);
-};
-printThis();
-
-//this - 3
-var printer = {
-  printThis: function () {
-    console.log('this2', this);
+var init = function () {
+  var hour = document.getElementById('hour');
+  var min = document.getElementById('min');
+  var sec = document.getElementById('sec');
+  var hourNumber = 0;
+  var minNumber = 0;
+  var secNumber = 0;
+  
+  function hourHandler() {
+    hour.innerHTML = addZero(hourNumber);
+    
   }
-};
-printer.printThis();
-
-//this - 3
-var printer = {
-  printThis: function () {
-    console.log('this1', this);
-    var innerPrintThis = function () {
-      console.log('this2', this);
-    };
-    innerPrintThis();
-    function lastPrintThis() {
-      console.log('this3', this);
+  
+  function minHandler() {
+    min.innerHTML = minNumber + 1;
+    if (minNumber === 60) {
+      hourNumber = hourNumber + 1;
+      hourHandler();
+      minNumber = 0;
     }
-    lastPrintThis();
+    min.innerHTML = addZero(minNumber);
   }
-};
-printer.printThis();
+  
+  function secHandler() {
+    secNumber = secNumber + 1;
+    if (secNumber === 60) {
+      minNumber = minNumber + 1;
+      minHandler();
+      secNumber = 0;
+    }
+    sec.innerHTML = addZero(secNumber);
+  }
+  
+  function addZero(num){
+    if (num < 10){
+      return '0'+ num;
+    }  
+    return num;
+  }
+  
+  function intervalhandler() {
+    secHandler();
+  }
 
-//콜백패턴 - 1
-document.body.addEventListener('click', function () {
-  console.log('body clicked!');
-});
-
-//콜백패턴 - 2
-setInterval(function () {
-  console.log('callback!!');
-}, 2000);
-
-//콜백 - 비동기 이벤트 리스너
-var printBody = function () {
-  console.log('body');
-};
-document.body.addEventListener('click', printBody);
-
-//콜백 - 타임아웃
-var clbk = function () {
-  console.log('callback!!');
-};
-setTimeout(clbk(), 3000);
-
-//함수 표현식(무명 함수 표현식), 익명 함수
-var sum = function (a, b) {
-  return a + b;
+  setInterval(intervalhandler, 1000);
 };
 
-//기명 함수 표현식 - 패턴 1
-var sum = function sum(a, b) {
-  return a + b;
-};
-
-//기명 함수 표현식 - 패턴 2
-var sum = function MathSum(a, b) {
-  return a + b;
-};
-
-//함수 선언식
-function sum(a, b) {
-  return a + b;
-}
-
-// 되도록 stack trace 를 위해 기명 함수 표현식을 사용하는 것을 추천.
+document.addEventListener('DOMContentLoaded', init);
