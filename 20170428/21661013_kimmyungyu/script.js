4c274b20c24a1d6162d83a416db65ed434cb7258
mmyuuu <mmy_uuu@naver.com>
2017-05-12 11:07:46 +0900

    수정

--- 20170428/21661013_kimmyungyu/script.js
+++ 20170428/21661013_kimmyungyu/script.js
@@ -1,24 +1,36 @@
-var number = document.getElementsByName('increment');
-var plus = document.getElementsByClassName('buttonList')[0].firstElementChild;
-var minus = document.getElementsByClassName('buttonList')[0].lastElementChild;
-var counter = document.getElementsByClassName('counter')[0];
+var init = function(){
+  //code here.
+  //1. element 찾기
+  var counter = document.getElementById('counter');
+  var plus = document.getElementById('plusButton');
+  var minus = document.getElementById('minusButton');
+  var counterNumber = 0;
 
-plus.addEventListener('click',function(){
-  for(var i = 0; i<number.length; ++i){
-    if (number[i].checked == true){
-      var textP = counter.innerHTML;
-      var countnum = number[i].value;
-      countnum = textP+'+'+number[i].value;
+  //카운터를 제어하는 함수
+  function counterHandler(num){
+    counter.innerHTML = num;
+  }
+  //플러스 버튼 클릭 시 실행
+  function clickPlusButton(){
+    counterNumber = counterNumber + getRadioValue();
+    counterHandler(counterNumber);
+  }
+  //마이너스 버튼 클릭 시 실행
+  function clickMinusButton(){
+    counterNumber = counterNumber - getRadioValue();
+    counterHandler(counterNumber);
+  }
+  //라디오 버튼의 선택 된 값을 찾는 기능
+  function getRadioValue(){
+    var radios = document.getElementsByName('increment');
+    for(var i = 0; i < radios.length; ++i){
+      var radio = radios[i];
+      if(radio.checked === true){
+        return parseInt(radio.value, 10);
+      }
     }
   }
-   counter.innerHTML = countnum;
-})
-minus.addEventListener('click',function(){
-  for(var i = 0; i<number.length; ++i){
-    if (number[i].checked == true){
-      var textM = counter.innerHTML;
-      var countnum2 = number[i].value;
-      countnum2 = textM+'-'+number[i].value;
-    }
-  }counter.innerHTML = countnum2;
-})
+  plus.addEventListener('click',clickPlusButton);
+  minus.addEventListener('click',clickMinusButton);
+}
+document.addEventListener('DOMContentLoaded', init);
