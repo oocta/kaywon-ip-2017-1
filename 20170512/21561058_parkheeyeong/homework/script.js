e38e637f6446687f667c14450b1f793cec714ac8
tongtong9513 <tongtong9513@gmail.com>
2017-05-16 19:58:32 +0900

    시작과 멈춤

---
+++ 20170512/21561058_parkheeyeong/homework/script.js
@@ -0,0 +1,47 @@
+var init = function(){
+  var hour = document.getElementById('hour');
+  var min = document.getElementById('min');
+  var sec = document.getElementById('sec');
+  var startbtn = document.getElementById('startButton');
+  var stopbtn = document.getElementById('stopButton');
+  var hourNumber = 0;
+  var minNumber = 0;
+  var secNumber = 0;
+
+  function hourHandler(){
+    hourNumber = hourNumber +1;
+    hour.innerHTML = addZero(hourNumber);
+  }
+  function minHandler(){
+    minNumber = minNumber +1;
+    if (minNumber ===60){
+      hourHandler();
+      minNumber = 0;
+    }
+    min.innerHTML = addZero(minNumber);
+  }
+  function secHandler(){
+    secNumber = secNumber +1;
+    if (secNumber ===60){
+      minHandler();
+      secNumber = 0;
+    }
+    sec.innerHTML = addZero(secNumber);
+  }
+  function addZero(num){
+    if(num<10){
+      return '0'+num;
+        }
+      return num;
+    }
+
+  function intervalHandler(){secHandler();}
+
+  var startTimer =setInterval(intervalHandler,1000);
+
+  startbtn.addEventListener('click',startTimer);
+  stopbtn.addEventListener('click',function(){clearInterval(startTimer);});
+
+
+};
+document.addEventListener('DOMContentLoaded',init);
