60a44f9a784e7af29fed3d337725ba290a96c29e
parksunjoo <qrktjswn@naver.com>
2017-05-18 17:00:12 +0900

    과제제출이요

--- 20170512/21561050_parksunjoo/watch.js
+++ 20170512/21561050_parksunjoo/watch.js
@@ -1,29 +1,47 @@
-var init = function () {
+var init = function(){
   var hour = document.getElementById('hour');
   var min = document.getElementById('min');
   var sec = document.getElementById('sec');
-  var hourNumber = '0';
-  var minNumber = '0';
-  var secNumber = '0';
 
+  var hourNumber =0;
+  var minNumber =0;
+  var secNumber =0;
   function hourHandler(){
-    hour.innterHTML = hourNumber;
+    hourNumber = hourNumber + 1;
+    if(hourNumber === 60){
+      hourNumber = 0;
+    }
+    hour.innerHTML = addZero(hourNumber);
   }
-
-  function minHandler() {
-    min.innterHTML = minNumber;
+  function minHandler(){
+    minNumber = minNumber + 1;
+    if(minNumber === 60){
+      hourHandler();
+      minNumber = 0;
+    }
+    min.innerHTML = addZero(minNumber);
   }
-
-  function secHandler() {
+  function secHandler(){
+    secNumber = secNumber + 1;
     if(secNumber === 60){
+      minHandler();
       secNumber = 0;
     }
-    hour.innterHTML = secNumber;
+   sec.innerHTML = addZero(secNumber);
   }
 
-  function intervalHandler() {
-  secHandler();
+  function addZero(num){
+    if (num < 10){
+      return '0'+ num;
+    }
+    return num;
+ }
+
+  function intervalHandler(){
+    secHandler();
   }
+
+setInterval(intervalHandler,20);
+
 };
-
-document.addEventListener('DOMContentLoded', init);
+document.addEventListener('DOMContentLoaded',init);
